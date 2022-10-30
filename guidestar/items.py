3f3a6ef4f3db336d685898e453cac723ddb6d361
Hendra Dwi Saputra <hendra@usa.com>
2022-10-30 16:47:38 +0700

    Model item organization name

--- guidestar/items.py
+++ guidestar/items.py
@@ -1,12 +1,6 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
+from unicodedata import name
 import scrapy
 
 
 class GuidestarItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
