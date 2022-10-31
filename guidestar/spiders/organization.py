0c365a482783764110ebcf5dfb5f7699b34130b7
Hendra Dwi Saputra <hendra@usa.com>
2022-10-31 09:21:00 +0700

    Extract field name in page 1 for example

--- guidestar/spiders/organization.py
+++ guidestar/spiders/organization.py
@@ -1,26 +1,35 @@
+import pandas as pd
 import re
 import scrapy
+
+from guidestar.items import GuidestarItem
 
 
 class OrganizationSpider(scrapy.Spider):
     name = 'organization'
+    cur_pages = 1
+    num_pages = 0
+    org_names = pd.DataFrame({'Name': []})
     allowed_domains = ['www.guidestar.org']
     start_urls = [
         'https://www.guidestar.org/nonprofit-directory/human-services/general-human-services/'
     ]
-    cur_pages = 1
-    num_pages = 0
 
     def start_requests(self):
         url = self.start_urls[0] + str(self.cur_pages) + '.aspx'
         yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
-        self.num_pages = response.xpath(
-            '//p[@class="centered"]/a[contains(.,"End")]/@href'
-        ).extract_first()
-        self.num_pages = int(re.findall(r'\d+', self.num_pages)[0])
-        if self.cur_pages < self.num_pages:
-            self.cur_pages += 1
-            next_pages = self.start_urls[0] + str(self.cur_pages) + '.aspx'
-            yield response.follow(next_pages, self.parse)
+        items = GuidestarItem()
+        data = response.xpath('//ul[@class="unindent half-space"]/li')
+        for dt in data:
+            items['name'] = dt.xpath('a/text()').extract()
+            yield items
+        # self.num_pages = response.xpath(
+        #     '//p[@class="centered"]/a[contains(.,"End")]/@href'
+        # ).extract_first()
+        # self.num_pages = int(re.findall(r'\d+', self.num_pages)[0])
+        # if self.cur_pages < self.num_pages:
+        #     self.cur_pages += 1
+        #     next_pages = self.start_urls[0] + str(self.cur_pages) + '.aspx'
+        #     yield response.follow(next_pages, self.parse)
